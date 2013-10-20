a0b6fbad01da8df3220d715ef8391e706e71c09b
Charles Lee <chug2k@gmail.com>
2013-10-20 16:06:16 -0700

    Doy. Rendering template even if current user is set.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,19 @@
 class SessionsController < ApplicationController
   def create
-    return if @current_player.present?
-
-    token = params[:token]
-    fb_user = FbGraph::User.me(token).fetch
-    fbuid = fb_user.raw_attributes[:id]
-    player = Player.find_by_fbuid(fbuid) || Player.initialize_from_facebook(fb_user)
-    Token.create(player: player, token: token)
+    if @current_player.blank?
+      token = params[:token]
+      fb_user = FbGraph::User.me(token).fetch
+      fbuid = fb_user.raw_attributes[:id]
+      player = Player.find_by_fbuid(fbuid) || Player.initialize_from_facebook(fb_user)
+      Token.create(player: player, token: token)
+    end
 
     # TODO(Charles): Maybe throw next_word in here.
-
     respond_to do |format|
       format.json {
-          render json: {points: 25}
-      }
+          render json: {points: 25, name: @current_player.name}
 
+      }
     end
   end
 end
