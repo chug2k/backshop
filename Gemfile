b93972b2fb2c20751178ddd7d90062a06c51da7b
Charles Lee <chug2k@gmail.com>
2013-11-03 17:17:28 -0800

    trying to get jquery things in and sweetest stinkies

--- Gemfile
+++ Gemfile
@@ -20,9 +20,7 @@ gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
-# Use jquery as the JavaScript library
 gem 'jquery-rails'
-# Use jquery mobile as the JavaScript library
 gem 'jquery_mobile_rails'
 
 gem 'haml-rails'
--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,7 +1,9 @@
 class LeaderboardsController < ApplicationController
   def index
     if params[:type] == 'photos'
-      @submissions = Submission.all.sort_by {|x| -x.score}
+      @submissions = Submission.all.sort_by {|x| -x.score}[0..50]
+    elsif params[:type] == 'badphotos'
+      @submissions = Submission.all.sort_by {|x| x.score}[0..50]
     else
       @players = Player.all.sort_by { |x| -x.score.values.sum }
     end
